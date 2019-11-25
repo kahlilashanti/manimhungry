8b5a167180a80a7536551afcc7fd399835f1f173
seetsdev <bushongo@protonmail.com>
2019-11-24 18:42:48 -0800

    added a function

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 //some javascript here
+
+// const firstNumber = process.argv[2];
+// const secondNumber = process.argv[3];
+
+// const answer = firstNumber * secondNumber
+// console.log(`the answer is ${answer}`)
+
+// function multiplyNumbers(x,y){
+//    return x*y
+// }
+
+//multiplyNumbers(1,2)
